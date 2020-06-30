775a0ddb4a5d1590bfd94471ae8d6595e6f1c475
Nicole Apsche <napsche@kgtiger.com>
2020-06-30 16:43:26 -0400

    adding error function and inquirer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,28 @@ var connection = mysql.createConnection({
     password: "",
     database: "bamazon"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err; 
+    start();
+})
+function start() {
+    inquirer
+        .prompt({
+            // name: "postOrBid",
+            // type: "list", 
+            // message: "Would you like to [POST] an auction or [BID] on an auction?",
+            // choices: ["POST", "BID", "EXIT"]
+        })
+        .then(function(answer) {
+            //based on their answer, either call the bid or post functions
+            // if (answer.postOrBid === "POST") {
+            //     postAuction();
+            // }
+            // else if(answer.postOrBid === "BID") {
+            //     bidAuction();
+            // } else{
+            //     RTCPeerConnection.end();
+            // }
+        });
+}
