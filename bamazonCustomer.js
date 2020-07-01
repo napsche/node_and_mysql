f803378aa2980995851b7be261679a6008dedf28
Nicole Apsche <napsche@kgtiger.com>
2020-07-01 13:28:52 -0400

    adding inquirer prompt to ask id of product and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,25 +11,22 @@ var connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
+    password: "Crouton!23",
     database: "bamazon"
   });
 
   connection.connect(function(err) {
     if (err) throw err; 
+    //display all items for sale!!!!
     connection.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err; 
         console.log("What would you like to buy?");
         console.log("-------------------------------------------------");
         console.log(result);
     })
-    // start();
+    start();
 });
-// module.exports = connection;
-//display all items for sale!!!!
-//in initial function, need to ask the customer 
-    //1. ID of product they want to buy
-    //2. How many units of the product they would like to buy
+
 //Application checks the database to see if there are enough units in stock
 //compare input to stock
 //if order number > units in stock, log phrase and prevent order from being placed
@@ -37,24 +34,29 @@ var connection = mysql.createConnection({
     //update database to reflect remaining amount of units
     //show customer total cost of their purchase
 
-// function start() {
-//     inquirer
-//         .prompt({
-//             name: "productToBuy",
-//             type: "input", 
-//             message: "What is the ID# of the product you would like to buy?",
-//         })
-//         .then(function(quantity) {
-//             inquirer
-//                 .prompt({
-//                     name: "quantity",
-//                     type: "number", 
-//                     message: "How many units would you like?"
-//                 })
-//                 .then(function() {
-
-//                 })
-//         });
-// }
+function start() {
+    inquirer
+    //in initial function, need to ask the customer 
+    //1. ID of product they want to buy
+        .prompt({
+            name: "item_id",
+            type: "input", 
+            message: "What is the ID# of the product you would like to buy?",
+        })
+        .then(function(quantity) {
+            inquirer
+             //2. How many units of the product they would like to buy
+                .prompt({
+                    name: "quantity",
+                    type: "number", 
+                    message: "How many units would you like?"
+                })
+                .then(function() {
+                    //Application checks the database to see if there are enough units in stock
+                    //compare input to stock
+                    // console.log("You would like " + quantity + "unit(s)!"); 
+                })
+        });
+}
 
 // //********************************PUT YOUR PASSWORD IN TO TEST***************
